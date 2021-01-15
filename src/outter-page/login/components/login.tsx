d668c96870d45e188f11cf76328915d7dcec2160
Qccccc's laptop <896853205@qq.com>
2021-01-15 22:19:42 +0800

    style: 修改格式

--- src/outter-page/login/components/login.tsx
+++ src/outter-page/login/components/login.tsx
@@ -1,20 +1,22 @@
-import React from "react";
-import axios from "axios";
-import md5 from "md5";
+import React from 'react';
 
-import { Input, Form, Button, message } from "antd";
-import { useRequest, useUnmount } from "ahooks";
-import "../style.css";
-import { KeyOutlined } from "@ant-design/icons";
-import * as APIS from "src/constants/api-constants";
+import { useRequest, useUnmount } from 'ahooks';
+import { Input, Form, Button, message } from 'antd';
+import { KeyOutlined } from '@ant-design/icons';
+import axios from 'axios';
+import md5 from 'md5';
+
+import '../style.css';
+import * as APIS from 'src/constants/api-constants';
 
 export default () => {
   let { loading, run, cancel } = useRequest(
     data => {
       let { password } = data;
+      
       return {
         url: APIS.AUTHORTION,
-        method: "GET",
+        method: 'GET',
         params: {
           password: md5(password),
         },
@@ -22,18 +24,19 @@ export default () => {
     },
     {
       manual: true,
-      onSuccess: _result => {
-        const token = _result.data.token;
+      onSuccess: result => {
+        const { token } = result.data;
+
         if (token) {
           // 添加全局请求头token
-          axios.defaults.headers.common["Authorization"] = token;
-          message.info("登陆成功");
+          axios.defaults.headers.common['Authorization'] = token;
+          message.info('登录成功');
         } else {
-          message.error("登陆失败");
+          message.error('登录失败');
         }
       },
-      onError: _error => {
-        message.error("登陆失败");
+      onError: () => {
+        message.error('登录失败');
       },
     }
   );
@@ -44,25 +47,24 @@ export default () => {
 
   return (
     <Form onFinish={values => run(values)}>
-      <Form.Item name="password">
+      <Form.Item name='password'>
         <Input
-          prefix={<KeyOutlined style={{ color: "rgba(0,0,0,.25)" }} />}
-          placeholder="密码"
-          type="password"
-          name="password"
-          className="password"
+          prefix={<KeyOutlined style={{ color: 'rgba(0,0,0,.25)' }} />}
+          placeholder='密码'
+          type='password'
+          name='password'
+          className='password'
         />
       </Form.Item>
       <Form.Item>
-        <div className="login-button-box">
+        <div className='login-button-box'>
           <Button
-            type="primary"
-            htmlType="submit"
+            type='primary'
+            htmlType='submit'
             loading={loading}
             style={{
-              width: "100%",
-            }}
-          >
+              width: '100%',
+            }}>
             登录
           </Button>
         </div>
