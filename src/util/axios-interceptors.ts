233c0dd16dfcf06925f6aaab711de6e77299e6df
Qccccc's laptop <896853205@qq.com>
2021-01-17 00:52:45 +0800

    feat: 折中最后使用了namespace的方法来避免msg重复

--- src/util/axios-interceptors.ts
+++ src/util/axios-interceptors.ts
@@ -22,38 +22,47 @@ export const responseLog = (response: any) => {
   console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
 };
 
-// TODO: 401 拦截器
-// history = createBrowserHistory(); history.push('/')
 export const responseSuccessMessage = (response: any) => {
+  const { msg } = response.data;
+  response.data = response.data.mainData;
+
   if (response.status === DominConfigs.RESPONSE_CODE.success) {
     // 查询200
-    if(response.message){
-      message.success(response.message);
+    if (msg) {
+      message.success(msg);
     }
   } else if (response.status === DominConfigs.RESPONSE_CODE.created) {
     // 创建201
-    if(response.message){
-      message.success(response.message);
+    if (msg) {
+      message.success(msg);
     }
   } else if (response.status === DominConfigs.RESPONSE_CODE.noContent) {
     // 更新204
-    if(response.message){
-      message.success(response.message);
+    if (msg) {
+      message.success(msg);
     }
   }
 };
 
+// TODO: 401 拦截器
+// history = createBrowserHistory(); history.push('/')
 export const responseErrorMessage = (error: any) => {
   const errorStatus = error.response.status;
+  const { msg } = error.response.data;
+
   if (errorStatus === DominConfigs.RESPONSE_CODE.error) {
     // 客户端请求失败400
-    message.error('密码错误！请重新输入');
+    if (msg) {
+      message.error(msg);
+    }
   } else if (errorStatus === DominConfigs.RESPONSE_CODE.unauthorized) {
     // 客户端请求失败401
     message.error('请进行身份验证！');
   } else if (errorStatus === DominConfigs.RESPONSE_CODE.unFind) {
     // 客户端请求失败404
-    message.error('您查找的信息不存在');
+    if (msg) {
+      message.error(msg);
+    }
   } else if (errorStatus === DominConfigs.RESPONSE_CODE.serviceError) {
     // 服务器解析错误500
     message.error('服务器内部异常，请稍后再试');
