6f96713a6593d75963238f9d53496ab9881c7c96
Qccccc's laptop <896853205@qq.com>
2021-01-19 16:06:53 +0800

    feat: 添加空白逻辑

--- src/page/feedback/components/statistic.tsx
+++ src/page/feedback/components/statistic.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 
 import { Statistic, Row, Col } from 'antd';
 import { useRequest, useUnmount } from 'ahooks';
@@ -9,17 +9,17 @@ export default () => {
   const [statisticData, setStaticticData] = useState({
     todayIncrease: 0,
     topEvaluation: {
-      topEvaluationMark: 0.0,
+      topEvaluationMark: '0.00',
       topSunSignName: '',
       topMoonSignName: '',
     },
     lowEvaluation: {
-      lowEvaluationMark: 0.0,
+      lowEvaluationMark: '0.00',
       lowSunSignName: '',
       lowMoonSignName: '',
     },
-    sumEvaluation: 0.0,
-    avgEvaluation: 0.0,
+    sumEvaluation: '0.00',
+    avgEvaluation: '0.00',
   });
 
   // 向后台获取统计数据
@@ -41,14 +41,20 @@ export default () => {
     }
   );
 
+  const statisticName = useCallback((sunSignName, moonSignName) => {
+    if (sunSignName && moonSignName) return `${sunSignName}/${moonSignName}`;
+    else return '--/--';
+  }, []);
+
   useUnmount(() => {
     cancel();
   });
+
   return (
     <Row gutter={20} style={{ marginBottom: 10 }}>
       <Col span={4}>
         <Statistic
-          title="本日增加数"
+          title='本日增加数'
           value={statisticData.todayIncrease}
           valueStyle={{ color: '#3f8600' }}
           prefix={<ArrowUpOutlined />}
@@ -56,33 +62,31 @@ export default () => {
       </Col>
       <Col span={4}>
         <Statistic
-          title="最高评价为"
+          title='最高评价为'
           valueStyle={{ color: '#3f8600' }}
           value={statisticData.topEvaluation.topEvaluationMark}
-          suffix={
-            statisticData.topEvaluation.topSunSignName +
-            '/' +
+          suffix={statisticName(
+            statisticData.topEvaluation.topSunSignName,
             statisticData.topEvaluation.topMoonSignName
-          }
+          )}
         />
       </Col>
       <Col span={4}>
         <Statistic
-          title="最低评价为"
+          title='最低评价为'
           valueStyle={{ color: '#cf1322' }}
           value={statisticData.lowEvaluation.lowEvaluationMark}
-          suffix={
-            statisticData.lowEvaluation.lowSunSignName +
-            '/' +
+          suffix={statisticName(
+            statisticData.lowEvaluation.lowSunSignName,
             statisticData.lowEvaluation.lowMoonSignName
-          }
+          )}
         />
       </Col>
       <Col span={4}>
-        <Statistic title="总评价数" value={statisticData.sumEvaluation} />
+        <Statistic title='总评价数' value={statisticData.sumEvaluation} />
       </Col>
       <Col span={4}>
-        <Statistic title="总评价平均数为" value={statisticData.avgEvaluation} />
+        <Statistic title='总评价平均数为' value={statisticData.avgEvaluation} />
       </Col>
     </Row>
   );
